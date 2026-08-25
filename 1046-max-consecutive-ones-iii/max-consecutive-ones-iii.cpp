fc2ceedc6c84a9330f10aacb313d30d4baef74bb
Suhani Baderia <suhanibaderia027@gmail.com>
2026-08-25 11:39:16 +0530

    Time: 0 ms (100.00%) | Memory: 69.7 MB (37.23%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -4,11 +4,11 @@ public:
      int r =0, l=0, maxlen =0, zeros =0, len;
      while(r<nums.size()){
         if(nums[r]==0 ) zeros++;
-        while(zeros > k){
+        if(zeros > k){
             if(nums[l] == 0)
                 zeros--;
                 l++;
-        }
+       }
         if(zeros <= k){
             len = r-l+1;
             maxlen = max(len, maxlen);
