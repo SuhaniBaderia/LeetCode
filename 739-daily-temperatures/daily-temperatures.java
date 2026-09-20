730b95dbd579ab5edc5a4ea5cacd07dce4c3fa4e
Suhani Baderia <suhanibaderia027@gmail.com>
2026-09-21 00:27:07 +0530

    Time: 52 ms (78.71%) | Memory: 107.1 MB (50.58%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,19 +1,24 @@
+import java.util.*;
+
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        HashMap<Integer,Integer> mp = new HashMap<>();
-        Stack <Integer> st = new Stack<>();
-        for (int i = 0; i < temperatures.length; i++) {
-            while (!st.empty() && temperatures[st.peek()] < temperatures[i]) {
-                 int index = st.pop();
-                mp.put(index, i - index);
-            }
-            st.push(i);
-        }
+
+        Stack<Integer> st = new Stack<>();
         int[] ans = new int[temperatures.length];
 
         for (int i = 0; i < temperatures.length; i++) {
-            ans[i] = mp.getOrDefault(i, 0);
+
+            while (!st.empty() &&
+                   temperatures[st.peek()] < temperatures[i]) {
+
+                int index = st.pop();
+
+                ans[index] = i - index;
+            }
+
+            st.push(i);
         }
+
         return ans;
     }
 }
